1b564ca97a50735066f9b0400159e5763212a878
Yukuo Cen <cenyk1230@qq.com>
2021-07-29 19:32:51 +0800

    QDropout for converter (#19)

--- actnn/actnn/__init__.py
+++ actnn/actnn/__init__.py
@@ -3,7 +3,7 @@ from . import ops
 from .conf import config, set_optimization_level
 from .dataloader import DataLoader
 from .layers import QConv1d, QConv2d, QConv3d, QConvTranspose1d, QConvTranspose2d, QConvTranspose3d, \
-    QBatchNorm1d, QBatchNorm2d, QBatchNorm3d, QLinear, QReLU, QSyncBatchNorm, QMaxPool2d
+    QBatchNorm1d, QBatchNorm2d, QBatchNorm3d, QLinear, QReLU, QDropout, QSyncBatchNorm, QMaxPool2d
 from .module import QModule
 from .qscheme import QScheme
 from .qbnscheme import QBNScheme
