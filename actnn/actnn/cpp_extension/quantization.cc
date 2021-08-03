9ca111ae8c30d3f6f41c3174656f4274cfdfc721
Yukuo Cen <cenyk1230@qq.com>
2021-08-03 16:38:39 +0800

    Fix QDropout

--- actnn/actnn/cpp_extension/quantization.cc
+++ actnn/actnn/cpp_extension/quantization.cc
@@ -30,7 +30,7 @@ Tensor act_quantized_relu_backward_cuda(Tensor grad_output, Tensor mask);
 
 // ActQuantizedDropout
 std::pair<Tensor, Tensor> act_quantized_dropout_forward_cuda(Tensor data, float dropout_p);
-Tensor act_quantized_dropout_backward_cuda(Tensor grad_output, Tensor mask);
+Tensor act_quantized_dropout_backward_cuda(Tensor grad_output, Tensor mask, float dropout_p);
 
 // ActQuantizedMaxPool2d
 std::pair<Tensor, Tensor> act_quantized_max_pool2d_forward_cuda(Tensor input,
@@ -131,12 +131,14 @@ class ActQuantizedDropout : public Function<ActQuantizedDropout> {
     Tensor output, mask;
     std::tie(output, mask) = act_quantized_dropout_forward_cuda(input, dropout_p);
     ctx->save_for_backward({mask});
+    ctx->saved_data["dropout_p"] = dropout_p;
     return output;
   }
 
   static tensor_list backward(AutogradContext *ctx, tensor_list grad_outputs) {
     auto saved = ctx->get_saved_variables();
-    return {act_quantized_dropout_backward_cuda(grad_outputs[0], saved[0]), Tensor()};
+    float dropout_p = float(ctx->saved_data["dropout_p"].toDouble());
+    return {act_quantized_dropout_backward_cuda(grad_outputs[0], saved[0], dropout_p), Tensor()};
   }
 };
 
--- actnn/actnn/layers.py
+++ actnn/actnn/layers.py
@@ -369,13 +369,16 @@ class QReLU(nn.Module):
         return ext_quantization.act_quantized_relu(input)
 
 
-class QDropout(nn.Module):
+class QDropout(nn.Dropout):
     def __init__(self, p=0.5):
-        super().__init__()
+        super().__init__(p=p)
         self.p = p
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
-        return ext_quantization.act_quantized_dropout(input, self.p)
+        if self.training:
+            return ext_quantization.act_quantized_dropout(input, self.p)
+        else:
+            return super(QDropout, self).forward(input)
 
 
 class QSyncBatchNorm(nn.SyncBatchNorm):
